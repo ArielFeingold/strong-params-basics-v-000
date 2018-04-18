f70dd6f2e4a72e6e8c2668d9c70c8604fe518738
Ariel Feingold <feingold.ariel@gmail.com>
2018-04-18 15:19:05 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,19 +16,19 @@ class PostsController < ApplicationController
 	  @post.save
 	  redirect_to post_path(@post)
 	end
-	 
+
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(post_params)
+	  @post.update(post_params(:title))
 	  redirect_to post_path(@post)
 	end
-	 
+
 	private
-	 
+
 	def post_params
 	  params.require(:post).permit(:title, :description)
 	end
-	
+
 	def edit
 	  @post = Post.find(params[:id])
 	end
